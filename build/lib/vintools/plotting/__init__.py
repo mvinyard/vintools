517a782212f30b88efa2057c03f0306f37323438
mvinyard <mvinyard@broadinstitute.org>
2021-07-28 14:10:34 +0000

    updated pypi package

--- build/lib/vintools/plotting/__init__.py
+++ build/lib/vintools/plotting/__init__.py
@@ -8,3 +8,5 @@ from .color_palettes import vin_colors
 from .color_palettes import shareseq_colors as share_seq
 
 from .share_seq_df_generation import shareseq_palette_df
+
+from ._histogram import _plot_histogram as histogram
