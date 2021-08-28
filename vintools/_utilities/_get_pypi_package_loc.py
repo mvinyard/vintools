fcb1870776300809b89818da1ff27c7cb8722e45
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-08-28 14:05:01 -0600

    Update _get_pypi_package_loc.py
    fixed read func

--- vintools/_utilities/_get_pypi_package_loc.py
+++ vintools/_utilities/_get_pypi_package_loc.py
@@ -11,7 +11,7 @@ def _get_pypi_package_loc(package='vintools'):
     tmp_info_file="info_{}.txt".format(signature)
     
     os.system('pip show {} > {}'.format(package, tmp_info_file))
-    df = pd.read_table("info.txt", sep = ": ", engine='python')
+    df = pd.read_table(tmp_info_file, sep = ": ", engine='python')
     package_loc = df.loc[df.Name == 'Location'].vintools.values.astype(str)[0]
     os.remove(tmp_info_file)
     
