9388471e7df66483540156d3249fc0e4cdf11433
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-08-28 14:00:04 -0600

    add custom signature to random file gen

--- vintools/_utilities/_get_pypi_package_loc.py
+++ vintools/_utilities/_get_pypi_package_loc.py
@@ -1,10 +1,15 @@
 import os
+import string
+import numpy as np
 import pandas as pd
 
-def _get_pypi_package_loc(package='vintools', tmp_info_file="info.txt"):
+def _get_pypi_package_loc(package='vintools'):
     
-    """"""
-
+    """"""    
+    
+    signature = "".join(np.random.choice(list(string.ascii_lowercase), 6))
+    tmp_info_file="info_{}.txt".format(signature)
+    
     os.system('pip show {} > {}'.format(package, tmp_info_file))
     df = pd.read_table("info.txt", sep = ": ", engine='python')
     package_loc = df.loc[df.Name == 'Location'].vintools.values.astype(str)[0]
