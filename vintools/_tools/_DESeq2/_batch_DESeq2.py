cbf929046566965b91c53947b36354e4ad689dc2
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-08-28 23:23:43 -0600

    update with unique tempdir for parallel running

--- vintools/_tools/_DESeq2/_batch_DESeq2.py
+++ vintools/_tools/_DESeq2/_batch_DESeq2.py
@@ -36,14 +36,17 @@ def _subset_input_dfs(df, df_meta, n_cancer_samples):
     return df_subset, df_meta_subset
 
 
-def _prep_DESeq2_input_data(dfe, df_meta, n_cancer_samples):
+def _prep_DESeq2_input_data(dfe, df_meta, n_cancer_samples, tmp_run_dir):
 
     """"""
 
     df_, dfm_ = _subset_input_dfs(dfe, df_meta, n_cancer_samples)
+    
+    path_data = os.path.join(tmp_run_dir, "gex_counts.csv")
+    path_meta = os.path.join(tmp_run_dir, "gex_meta.csv")
 
-    df_.to_csv("./tmp/gex_counts.csv")
-    dfm_.to_csv("./tmp/gex_meta.csv", index_label=False)
+    df_.to_csv(path_data)
+    dfm_.to_csv(path_meta, index_label=False)
 
 
 def _run_DESeq2(
@@ -55,14 +58,17 @@ def _run_DESeq2(
     software_dir = os.path.dirname(_get_pypi_package_loc())
     path = os.path.join(software_dir, "vintools/vintools/_tools/_DESeq2/DESeq2.R")
     
-
+    tmp_run_dir="tmp_{}".format("".join(np.random.choice(list(string.ascii_lowercase), 6)))
+    
     _flexible_mkdir(outs_dir)
-    _flexible_mkdir("tmp")
-    _prep_DESeq2_input_data(dfe, df_meta, n_cancer_samples)
+    _flexible_mkdir(tmp_run_dir)
+    _prep_DESeq2_input_data(dfe, df_meta, n_cancer_samples, tmp_run_dir)
 
     executable = " ".join(["Rscript", path, outs_dir, run_name])
     print(executable)
     os.system(executable)
+    
+    os.system("rm -r {}".format(tmp_run_dir))
 
 
 def _run_batch_DESeq2(dfe, df_meta, n_iters, n_cancer_samples):
