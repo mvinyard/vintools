02e921b5166d49df7357f89cfc2419500b1cfcf2
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-10-06 14:00:11 -0400

    add return line and documentation

--- vintools/_utilities/_system_utils/_use_n_cores.py
+++ vintools/_utilities/_system_utils/_use_n_cores.py
@@ -3,7 +3,24 @@ from ._fetch_cpu_count import _fetch_cpu_count
 
 def _use_n_cores(n_cores):
     
-    """"""
+    """
+    Given a number of desired cores to be used for a process, this function checks how many cores are available and 
+    returns the whatever value is greater, cores available or cores requested. 
+    
+    Parameters:
+    -----------
+    n_cores
+        number of cores desired to be used for a given process
+        
+    Returns:
+    --------
+    n_cores
+        number of cores to be used for a given process
+        type: int
+    
+    Notes:
+    ------
+    """
     n_cpus_available = _fetch_cpu_count()
     
     if n_cores > n_cpus_available:
@@ -11,3 +28,5 @@ def _use_n_cores(n_cores):
         n_cores = n_cpus_available
     else:
         print("Using {} cores...".format(n_cores))
+    
+    return n_cores
