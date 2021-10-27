496889c8a89a41cc481b6903453de23bd4a50500
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-10-27 11:48:01 -0400

    Create _update_dict.py

---
+++ vintools/_utilities/_data_utils/_update_dict.py
@@ -0,0 +1,12 @@
+
+# _update_dict.py
+__module_name__ = "_update_dict.py"
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
+
+def _update_dict(self, **kwargs):
+
+    for key, value in kwargs.items():
+        self.X[key] = value
+
+    return self
